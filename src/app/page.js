805c596d3dbcc33e915cdfac75fa7a3f5bef7aa1
Priyadarsh S S <priyadarsh2001@gmail.com>
2024-01-15 01:30:15 +0530

    feat: finish gallery

--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,17 @@
-import styles from './page.module.scss'
-import Image from 'next/image'
+import React from 'react'
 
-const images = [
-  '1.jpg',
-  '2.jpg',
-  '3.jpg',
-  '4.jpg',
-  '5.jpg',
-  '6.jpg',
-  '7.jpg',
-  '8.jpg',
-  '9.jpg',
-  '10.jpg',
-  '11.jpg',
-  '12.jpg',
-]
-
-export default function Home() {
+const Page = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.gallery}>
-        <Column images={[images[0], images[1], images[2]]} />
-        <Column images={[images[3], images[4], images[5]]} />
-        <Column images={[images[6], images[7], images[8]]} />
-        <Column images={[images[9], images[10], images[11]]} />
-      </div>
-    </main>
-  )
-}
-
-const Column = ({ images }) => {
-  return (
-    <div className={styles.column}>
-      {images.map((src, index) => {
-        return (
-          <div key={index} className={styles.imageContainer}>
-            <Image src={`/images/${src}`} fill alt='image' />
-          </div>
-        )
-      })}
+    <div>
+      <h1>Page</h1>
+      {/* give a link below with the name gallery */}
+      <a href="/gallery">Gallery</a>
+      {/* give more examples */}
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+      
     </div>
   )
 }
+
+export default Page
